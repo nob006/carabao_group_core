f3d1fef9752789b9ad8c7bb18f2f8f10cda89bd6
nob <nop.gpk@gmail.com>
2020-09-14 14:03:31 +0700

    creage project with core project

---
+++ core/lib/core.dart
@@ -0,0 +1,11 @@
+library core;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  String getFromCore(){
+    return "From Core Packet";
+  }
+}
